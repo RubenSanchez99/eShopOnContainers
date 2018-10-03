c0e7ce068841acd15e6c38189153860884573db9
Rubén Sánchez <luis.sanchezma@uanl.mx>
2018-10-02 23:49:01 -0500

    Added CRUD functionality

---
+++ src/Services/Catalog/Catalog.API/Infrastructure/EntityConfigurations/CatalogTypeEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Catalog.API.Model;
+
+namespace Catalog.API.Infrastructure.EntityConfigurations
+{
+    public class CatalogTypeEntityTypeConfiguration : IEntityTypeConfiguration<CatalogType>
+    {
+        public void Configure(EntityTypeBuilder<CatalogType> builder)
+        {
+            builder.ToTable("CatalogType");
+
+            builder.HasKey(ci => ci.Id);
+
+            builder.Property(ci => ci.Id)
+               .ForSqlServerUseSequenceHiLo("catalog_type_hilo")
+               .IsRequired();
+
+            builder.Property(cb => cb.Type)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}

f4ee48c25d5799c98d6ca1dc07b924778b704c91
Rubén Sánchez <luis.sanchezma@uanl.mx>
2018-11-27 13:18:55 -0600

    Agregados eventos de integración

---
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/Events/OrderStatusChangedToAwaitingValidationIntegrationEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace eShopOnContainers.Services.IntegrationEvents.Events
+{
+    using System.Collections.Generic;
+
+    public class OrderStatusChangedToAwaitingValidationIntegrationEvent
+    {
+        public string OrderId { get; }
+        public string OrderStatus { get; }
+        public string BuyerName { get; }
+        public IEnumerable<OrderStockItem> OrderStockItems { get; }
+
+        public OrderStatusChangedToAwaitingValidationIntegrationEvent(string orderId, string orderStatus, string buyerName,
+            IEnumerable<OrderStockItem> orderStockItems)
+        {
+            OrderId = orderId;
+            OrderStockItems = orderStockItems;
+            OrderStatus = orderStatus;
+            BuyerName = buyerName;
+        }
+    }
+
+    public class OrderStockItem
+    {
+        public int ProductId { get; }
+        public int Units { get; }
+
+        public OrderStockItem(int productId, int units)
+        {
+            ProductId = productId;
+            Units = units;
+        }
+    }
+}

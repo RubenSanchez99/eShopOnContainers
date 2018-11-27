3c59d7943f8c8a38a0dff6623810281c9b128ff5
Rubén Sánchez <luis.sanchezma@uanl.mx>
2018-11-27 15:35:02 -0600

    Agregado MassTransit y EventFlow

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -10,6 +10,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using EventFlow;
+using EventFlow.Extensions;
+using EventFlow.Autofac.Extensions;
+using EventFlow.Configuration;
+using EventFlow.MetadataProviders;
+using EventFlow.TestHelpers;
+using EventFlow.AspNetCore.MetadataProviders;
+using EventFlow.AspNetCore.Extensions;
+using EventFlow.Aspnetcore.Middlewares;
+using Ordering.API.Services;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
+using Microsoft.Extensions.Hosting;
 
 namespace Ordering.API
 {
@@ -23,13 +38,39 @@ namespace Ordering.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            var containerBuilder = new ContainerBuilder();
+
+            var container = EventFlowOptions.New
+                .UseAutofacContainerBuilder(containerBuilder)
+                .AddDefaults(typeof(Startup).Assembly)
+                //.AddEntityFrameworkReadModel()
+                .AddAspNetCoreMetadataProviders();
+
+            containerBuilder.Register(c =>
+            {
+                return Bus.Factory.CreateUsingRabbitMq(sbc => 
+                    sbc.Host(new Uri("rabbitmq://rabbitmq"), h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    })
+                );
+            })
+            .As<IBusControl>()
+            .As<IPublishEndpoint>()
+            .SingleInstance();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            containerBuilder.Populate(services);
+
+            return new AutofacServiceProvider(containerBuilder.Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -41,6 +82,7 @@ namespace Ordering.API
             }
 
             app.UseHttpsRedirection();
+            app.UseMiddleware<CommandPublishMiddleware>();
             app.UseMvc();
         }
     }

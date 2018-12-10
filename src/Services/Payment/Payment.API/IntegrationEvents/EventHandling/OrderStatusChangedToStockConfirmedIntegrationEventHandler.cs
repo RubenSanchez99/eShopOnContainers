e8e8a4b94c54f3e6f6744fbafa81e125ae9ad16c
Rubén Sánchez <luis.sanchezma@uanl.mx>
2018-12-10 15:13:40 -0600

    Agregado servicio Payment

---
+++ src/Services/Payment/Payment.API/IntegrationEvents/EventHandling/OrderStatusChangedToStockConfirmedIntegrationEventHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace Payment.API.IntegrationEvents.EventHandling
+{
+    using MassTransit;
+    using Microsoft.Extensions.Options;
+    using eShopOnContainers.Services.IntegrationEvents.Events;
+    using System.Threading.Tasks;
+
+    public class OrderStatusChangedToStockConfirmedIntegrationEventHandler : 
+        IConsumer<OrderStatusChangedToStockConfirmedIntegrationEvent>
+    {
+        private readonly IPublishEndpoint _endpoint;
+        private readonly PaymentSettings _settings;
+
+        public OrderStatusChangedToStockConfirmedIntegrationEventHandler(IPublishEndpoint endpoint, 
+            IOptionsSnapshot<PaymentSettings> settings)
+        {
+            _endpoint = endpoint;
+            _settings = settings.Value;
+        }
+
+        public async Task Consume(ConsumeContext<OrderStatusChangedToStockConfirmedIntegrationEvent> context)
+        {
+            //Business feature comment:
+            // When OrderStatusChangedToStockConfirmed Integration Event is handled.
+            // Here we're simulating that we'd be performing the payment against any payment gateway
+            // Instead of a real payment we just take the env. var to simulate the payment 
+            // The payment can be successful or it can fail
+
+            if (_settings.PaymentSucceded)
+            {
+                await _endpoint.Publish(new OrderPaymentSuccededIntegrationEvent(context.Message.OrderId));
+            }
+            else
+            {
+                await _endpoint.Publish(new OrderPaymentFailedIntegrationEvent(context.Message.OrderId));
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/Payment/Payment.API/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Payment.API.IntegrationEvents.EventHandling;
+using eShopOnContainers.Services.IntegrationEvents.Events;
+using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Payment.API.Services;
+using Microsoft.Extensions.Hosting;
+
+namespace Payment.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            Configuration = configuration;
+            _loggerFactory = loggerFactory;
+        }
+
+        public IConfiguration Configuration { get; }
+        private ILoggerFactory _loggerFactory { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {            
+            services.Configure<PaymentSettings>(Configuration);
+
+            var containerBuilder = new ContainerBuilder();
+
+            services.AddScoped<IHostedService, MassTransitHostedService>();
+            services.AddScoped<OrderStatusChangedToStockConfirmedIntegrationEventHandler>();
+
+            containerBuilder.Register(c =>
+            {
+                var busControl = Bus.Factory.CreateUsingRabbitMq(sbc => 
+                {
+                    var host = sbc.Host(new Uri("rabbitmq://rabbitmq"), h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    sbc.ReceiveEndpoint(host, "validate_payment_queue", e => 
+                    {
+                        e.Consumer<OrderStatusChangedToStockConfirmedIntegrationEventHandler>(c);
+                    });
+                    sbc.UseExtensionsLogging(_loggerFactory);
+                });
+                var consumeObserver = new ConsumeObserver(_loggerFactory.CreateLogger<ConsumeObserver>());
+                busControl.ConnectConsumeObserver(consumeObserver);
+
+                var sendObserver = new SendObserver(_loggerFactory.CreateLogger<SendObserver>());
+                busControl.ConnectSendObserver(sendObserver);
+
+                return busControl;
+            })
+            .As<IBusControl>()
+            .As<IPublishEndpoint>()
+            .SingleInstance();
+
+
+            containerBuilder.Populate(services);
+            return new AutofacServiceProvider(containerBuilder.Build());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var pathBase = Configuration["PATH_BASE"];
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
+            }
+        }
+    }
+}

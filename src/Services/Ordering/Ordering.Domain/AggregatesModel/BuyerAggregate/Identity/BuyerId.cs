1d36793d641fef4800634bf2489060722f49dab4
Rubén Sánchez <luis.sanchezma@uanl.mx>
2019-01-08 15:46:59 -0600

    Uso de funcionalidad default en BuyerId

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/Identity/BuyerId.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/Identity/BuyerId.cs
@@ -6,45 +6,10 @@ using Newtonsoft.Json;
 namespace Ordering.Domain.AggregatesModel.BuyerAggregate.Identity
 {
     [JsonConverter(typeof(SingleValueObjectConverter))]
-    public class BuyerId : IIdentity
+    public class BuyerId : Identity<BuyerId>
     {
-        private Guid Id;
-
-        public BuyerId(Guid guid)
+        public BuyerId(string value) : base(value)
         {
-            Id = guid;
-        }
-
-        [JsonConstructor]
-        public BuyerId(string Value)
-        {
-            Id = Guid.Parse(Value);
-        }
-
-        public string Value => Id.ToString();
-
-        public static bool operator== (BuyerId obj1, BuyerId obj2)
-        {
-            return (obj1.Id == obj2.Id);
-        }
-
-        public static bool operator!= (BuyerId obj1, BuyerId obj2)
-        {
-            return (obj1.Id != obj2.Id);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || GetType() != obj.GetType())
-                return false;
-
-            var b2 = (BuyerId)obj;
-            return (this.Id == b2.Id);
-        }
-
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
         }
     }
 }
